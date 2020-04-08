152aecc6d1dc74b31aec39b49824a7415b548355
Weikai Zhang <weikaizhang@Weikai-Zhang-MBP15.local>
2020-04-08 16:29:56 -0700

    Update fix

--- src/browser/components/StaticInfo.js
+++ src/browser/components/StaticInfo.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 // eslint-disable-next-line
 let context = React.createContext({})
+if (typeof document !== 'undefined') {
+  context = React.createContext(window.__routeInfo)
+}
 
 export default context
 
--- src/static/components/BodyWithMeta.js
+++ src/static/components/BodyWithMeta.js
@@ -3,6 +3,14 @@ import { pathJoin, makePathAbsolute } from '../../utils'
 
 const REGEX_FOR_SCRIPT = /<(\/)?(script)/gi
 
+const generateRouteInformation = () => ({
+  __html: `
+    window.__routeInfo = ${JSON.stringify({}).replace(
+      REGEX_FOR_SCRIPT,
+      '<"+"$1$2'
+    )};`,
+})
+
 // Not only do we pass react-helmet attributes and the app.js here, but
 // we also need to  hard code site props and route props into the page to
 // prevent flashing when react mounts onto the HTML.
@@ -16,6 +24,12 @@ export const makeBodyWithMeta = ({
 }) => ({ children, ...rest }) => (
   <body {...head.bodyProps} {...rest}>
     {children}
+    {!route.redirect && (
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={generateRouteInformation(embeddedRouteInfo)}
+      />
+    )}
     {!route.redirect &&
       clientScripts.map(script => (
         <script
